2494416ec7024cd79246723d6880bddf3f14f680
Wasanjith <wasakodi79@gmail.com>
2024-08-19 11:57:52 +0530

    Changes to be committed: 	modified:   backend/controllers/auth.controller.js 	new file:   backend/middleware/protectRoute.js 	modified:   backend/routes/auth.routes.js 	modified:   backend/server.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { getMe, signup, login, logout } from "../controllers/auth.controller.js";
+import { protectRoute  } from "../middleware/protectRoute.js"
+
 
 const router = express.Router();
 
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
-
-router.post("/logout", logout);
-
 router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 console.log(process.env.MONGO_URI); // Corrected from MOGO_URI to MONGO_URI
 app.use(express.json()); // to pass req.body
 app.use(express.urlencoded({ extended: true})); // to pass from data(urlencoded)
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
