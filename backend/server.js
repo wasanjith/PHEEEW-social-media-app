84107a882742b49c9e038eb7ef41dd1bba0fffcf
Wasanjith <wasakodi79@gmail.com>
2024-08-19 21:24:38 +0530

    Changes to be committed: 	new file:   backend/controllers/user.controller.js 	new file:   backend/routes/user.routes.js 	modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
@@ -13,6 +14,7 @@ app.use(express.json()); // to pass req.body
 app.use(express.urlencoded({ extended: true})); // to pass from data(urlencoded)
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
