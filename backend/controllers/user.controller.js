78d1f05aeb4e0b9172b8c6c9f173dc6489445b94
Wasanjith <wasakodi79@gmail.com>
2024-08-21 02:52:29 +0530

    update user section completed

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,9 @@
 import bcrypt from "bcryptjs";
+import {v2 as cloudinary } from "cloudinary";
+
 import Notification from "../modles/notification.js";
 import User from "../modles/user.model.js";
+
 
 
 export const getUserProfile = async (req, res) => {
@@ -61,3 +64,102 @@ export const followUnfollowUser = async (req, res) => {
     }
 };
 
+export const getSuggestedUsers = async (req, res) => {
+	try {
+		const userId = req.user._id;
+
+		const usersFollowedByMe = await User.findById(userId).select("following");
+
+		const users = await User.aggregate([
+			{
+				$match: {
+					_id: { $ne: userId },
+				},
+			},
+			{ $sample: { size: 10 } },
+		]);
+
+		// 1,2,3,4,5,6,
+		const filteredUsers = users.filter((user) => !usersFollowedByMe.following.includes(user._id));
+		const suggestedUsers = filteredUsers.slice(0, 4);
+
+		suggestedUsers.forEach((user) => (user.password = null));
+
+		res.status(200).json(suggestedUsers);
+	} catch (error) {
+		console.log("Error in getSuggestedUsers: ", error.message);
+		res.status(500).json({ error: error.message });
+	}
+};
+
+export const updateUser = async (req, res) => {
+    const { fullName , email , username , currentPassword , newPassword , bio ,link } = req.body;
+    let { profileImg , coverImg} = req.body;
+
+    const userId = req.user._id;
+
+    try {
+        let user = await User.findById(userId);
+        if(!user) return res.status(404).json({message: "User not found"});
+
+        if(!user.currentPassword){
+            return res.status(400).json({error: "please enter the valid password"});
+        }
+
+        if(currentPassword === currentPassword){
+            return res.status(400).json({message: "current password is correct now add new password filed"})
+        }
+
+        if((!newPassword && currentPassword )||(!currentPassword && newPassword)){
+            return res.status(400).json({message: "please provide both current paswword and new password"});
+        }
+
+        if(currentPassword && newPassword){
+            const isMath = await bcrypt.compare(currentPassword, user.password);
+            if(!isMath) return res.sataus(400).json({error: "current password is incorrect"});
+            if(newPassword.length < 6) {
+                return res.status(400).json({error: "Password must be at least 6 characters long"});
+            }
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(newPassword, salt);
+        }
+        if(profileImg){
+
+            if(user.profileImg){
+                await cloudinary.uploader.destroy(user.profileImg.split("/").pop().split(".")[0]);
+            }
+           const uploadedResponse = await cloudinary.uploader.upload(profileImg)
+           profileImg = uploadedResponse.secure_url;
+
+        }
+        if(coverImg){
+            
+            if(user.coverImg){
+                await cloudinary.uploader.destroy(user.coverImg.split("/").pop().split(".")[0]);
+            }
+           const uploadedResponse = await cloudinary.uploader.upload(coverImg)
+           coverImg = uploadedResponse.secure_url;
+
+        }
+
+        user.fullName = fullName || user.fullName;
+        user.email = email || user.email;
+        user.username = username || user.username;
+        user.bio = bio || user.bio;
+        user.link = link || user.link;
+        user.profileImg = profileImg || user.profileImg;
+        user.coverImg = coverImg || user.coverImg;
+
+        user = await user.save();
+        user.password = null;
+
+        return res.status(200).json(user);
+
+
+
+    } catch (error) {
+        console.log("Error is updateUser: " , error.message);
+        res.status(500).json({ error: error.message});
+        
+    }
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
-import { getUserProfile} from '../controllers/user.controller.js';
-import { followUnfollowUser} from '../controllers/user.controller.js';
+import { getUserProfile , getSuggestedUsers , followUnfollowUser , updateUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get("/profile/:username",protectRoute,getUserProfile);
-// router.get("/suggested",protectRoute,getUserProfile);
+router.get("/suggested",protectRoute,getSuggestedUsers);
 router.post("/follow/:id",protectRoute,followUnfollowUser);
-// router.post("/update",protectRoute,updateUserProfile);
+router.post("/update",protectRoute,updateUser);
 
 export default router;
