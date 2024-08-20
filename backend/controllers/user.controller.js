139c4bb5a406b3cf1132ef1664ca1be8dc3a041a
Wasanjith <wasakodi79@gmail.com>
2024-08-20 14:09:47 +0530

    modified:   backend/controllers/user.controller.js 	new file:   backend/modles/notification.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,10 +1,12 @@
-import User from "../modles/user.model.js"
+import User from "../modles/user.model.js";
+import mongoose from "mongoose";
+import Notification from "../modles/notification.js";
 
 export const getUserProfile = async (req, res) => {
     const {username} = req.params;
 
     try {
-        const user = await user.findOne({username}).select("-password");
+        const user = await User.findOne({username}).select("-password");
         if(!user)  return res.status(404).json({error: "User not found"});
         
         res.status(200).json(user);
@@ -24,21 +26,32 @@ export const followUnfollowUser = async (req, res) => {
        if(id === req.user._id.toString()) {
            return res.status(400).json({ error: "You can't follow/unfollow yourself"});
        }
-       if(!userToModify || currentUser) return res.status(400).json({error: "User not found"});
+       if(!userToModify || !currentUser) return res.status(400).json({error: "User not found"});
 
        const isFollowing = currentUser.following.includes(id);
 
        if(isFollowing){
          //Unfollow the user
          await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
-         await User.findByIdAndDelete(req.user._id, { $pull: { following: id } });
+         await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+        // TODO: return the id of the usar as a response
+
          res.status(200).json({message: "User unfollowed successfully"});
        }else{
         //Follow the user
         await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
-        await User.findByIdAndDelete(req.user._id, { $push: { following: id } });
+        await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
         // Send notification to the user
-        res.status(200).json({ mwssage: "User followed successfully"});
+        const newNotification = new Notification({
+            type: "follow",
+            from: "req.user._id",
+            to: userToModify._id,        
+        });
+
+        await newNotification.save();
+
+        // TODO: return the id of the user as a response
+        res.status(200).json({ message: "User followed successfully"});
        }
         
        
---
+++ backend/modles/notification.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+// import User from "../controllers/user.controller";
+
+const notificationSchema = new mongoose.Schema({
+    from:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    to:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    type:{
+        type: String,
+        required: true,
+        enum: ["follow", "like"] // add later comment and mentions
+    },
+    read:{
+        type: Boolean,
+        default: false
+    },
+    },{timestamps: true})
+
+    const Notification = mongoose.model("Notification", notificationSchema);
+
+    export default Notification;
