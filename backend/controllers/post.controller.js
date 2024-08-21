f17f4fe0c2147fac524aa64a96c77594e2f4f49f
Wasanjith <wasakodi79@gmail.com>
2024-08-21 23:22:49 +0530

    Finsish the all user auth post controllers

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -103,10 +103,12 @@ export const likeUnlikePost = async (req, res) => {
 		if(userLikedPost){
 			//Unlike post
 			await Post.updateOne({_id:postId}, {$pull: {likes: userId}});
+			await User.updateOne({_id:userId}, {$pull: {likedPosts: postId}});
 			res.status(200).json({message: "Post unliked successfully"})
 		}else{
 			//like post
 			post.likes.push(userId);
+			await User.updateOne({_id:userId}, {$push: {likedPosts: postId}});
 			await post.save();
 
 			const notification = new Notification({
@@ -148,4 +150,80 @@ export const getAllPosts = async (req, res) => {
 		console.log("Error in getAllPosts controller: ", error);
 		res.status(500).json({ error: "Internal server error" });
 	}
+};
+
+export const getLikedPosts = async (req, res) => {
+	const userId = req.params.id;
+
+	try {
+		const user = await User.findById(userId);
+		if (!user) return res.status(404).json({ error: "User not found" });
+
+		const likedPosts = await Post.find({ _id: { $in: user.likedPosts } })
+			.populate({
+				path: "user",
+				select: "-password",
+			})
+			.populate({
+				path: "comments.user",
+				select: "-password",
+			});
+
+		res.status(200).json(likedPosts);
+	} catch (error) {
+		console.log("Error in getLikedPosts controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
+
+export const getFollowingPosts = async (req, res) => {
+    try {
+		const userId = req.user._id;
+		const user = await User.findById(userId);
+		if (!user) return res.status(404).json({ error: "User not found" });
+
+		const following = user.following;
+
+		const feedPosts = await Post.find({ user: { $in: following } })
+					.sort({ createdAt: -1 })
+					.populate({
+						path: "user",
+						select: "-password",
+					})
+					.populate({
+						path: "comments.user",
+						select: "-password",
+					});
+
+		res.status(200).json(feedPosts);
+	} catch (error) {
+		console.log("Error in getFollowingPosts controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
+
+export const getUserPosts = async (req, res) => {
+	try {
+		const { username } = req.params;
+
+		const user = await User.findOne({ username });
+		if (!user) return res.status(404).json({ error: "User not found" });
+
+		const posts = await Post.find({ user: user._id })
+			.sort({ createdAt: -1 })
+			.populate({
+				path: "user",
+				select: "-password",
+			})
+			.populate({
+				path: "comments.user",
+				select: "-password",
+			});
+
+		res.status(200).json(posts);
+	} catch (error) {
+		console.log("Error in getUserPosts controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
+    
 };
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,14 +1,18 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost , deletePost , commentOnPost , likeUnlikePost  , getAllPosts} from "../controllers/post.controller.js";
+import { createPost , deletePost , commentOnPost , likeUnlikePost  , getAllPosts , getLikedPosts , getFollowingPosts , getUserPosts} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
 router.post("/create", protectRoute, createPost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
 router.delete("/:id", protectRoute, deletePost);
+
 
 export default router;
 
